003c6905009df11bd235fd49f3693fa13be74495
CrabProgrammer <87841420+IT-Jaden-John@users.noreply.github.com>
2022-06-08 18:48:31 +0800

    Add files via upload

---
+++ run/terminal.py
@@ -0,0 +1,49 @@
+#Ocean Terminal
+#By Dragon Ocean 2022
+import os,platform,time
+admin="false"
+ospl=platform.system()
+def version():
+	print("Ocean Terminal 1.0.0 By Dragon Ocean")
+def ocexit():
+	exit()
+def changemode():
+	if admin=="false":
+		print("ocean@ocean: $ ",end="")
+	elif admin=="true":
+		print("root@ocean: $ ",end="")
+def clearscreen():
+	if ospl=="Windows":
+		os.system("cls")
+	elif ospl=="Linux":
+		os.system("clear")
+	else:
+		os.system("clear")
+def gettime():
+	getnow=time.localtime()
+	nowtime=time.strftime("%Y-%m-%d %H:%M:%S")
+	print(nowtime)
+while True:
+	changemode()
+	todo=input()
+	if todo=="version":
+		version()
+	elif todo=="exit":
+		if admin=="true":
+			admin="false"
+		else:
+			ocexit()
+	elif todo=="su":
+		admin="true"
+	elif todo=="sudo -i":
+		admin="true"
+	elif todo=="su root":
+		admin="true"
+	elif todo=="clear":
+		clearscreen()
+	elif todo=="time":
+		gettime()
+	elif todo=="date":
+		gettime()
+	else:
+		print("Ocean:Command "+str(todo)+" not found.")
